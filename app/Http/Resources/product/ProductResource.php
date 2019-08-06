1ca8baefc4fc1bae3ae4af0308b0873d8bc42b97
RsRuman <rsbdruman@gmail.com>
2019-08-06 18:29:13 +0600

    Modified product detail

--- app/Http/Resources/product/ProductResource.php
+++ app/Http/Resources/product/ProductResource.php
@@ -16,10 +16,21 @@ class ProductResource extends Resource
     {
         return [
             'name' => $this->name,
-            'description' => $this->detail,
+            'description' => $this->details,
             'price' => $this->price,
-            'stock' => $this->stock,
-            'discount' => $this->discount
+            'stock' => $this->stock == 0 ? 'Stock is empty!' : $this->stock,
+            'discount' => $this->discount,
+            'totalPrice' => round((1- ($this->discount/100)) * $this->price, 2),
+
+            'rating' => $this->reviews->count()>0
+                ? // if
+                round($this->reviews->sum('star')/ $this->reviews->count())
+                : // else
+                'No rating yet!',
+
+            'href' =>[
+                'reviews' => route('reviews.index', $this->id)
+            ]
         ];
     }
 }
